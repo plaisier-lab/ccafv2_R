cfbad2456f66259e29d2ff3bb72f92d3c159864b
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2023-12-14 14:25:10 -0700

    Changing print statements

--- ccAFv2/R/ccAFv2.R
+++ ccAFv2/R/ccAFv2.R
@@ -21,8 +21,7 @@ PredictCellCycle = function(seurat1, cutoff=0.5, species='human', gene_id='ensem
     # Load model and marker genes
     ccAFv2 = keras::load_model_hdf5(system.file('extdata', 'ccAFv2_model.h5', package='ccAFv2'))
     mgenes = read.csv(system.file('extdata', 'ccAFv2_genes.csv', package='ccAFv2'), header=TRUE, row.names=1)[,paste0(species,'_',gene_id)]
-    cat(mgenes)
-    cat(length(mgenes))
+    cat(paste0(length(mgenes),'\n'))
     
     # Subset data marker genes to marker genes included in classification
     cat('Subset\n')
